aaa642fa72e87e2a59f15b66146aa52cc201ab31
Alexandr Gussakov <49525149+AlexKeyMAD@users.noreply.github.com>
2023-07-17 11:26:45 +0600

    Task 3.1
    finish task

---
+++ Module_03/Task 1. Even or odd number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1.Even_or_odd_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            var str = Console.ReadLine();
+
+            int num = int.Parse(str);
+
+            if (num % 2 == 0) Console.WriteLine($"Число {num} - четное");
+            else Console.WriteLine($"Число {num} - нечетное");
+
+            Console.ReadKey();
+        }
+    }
+}
