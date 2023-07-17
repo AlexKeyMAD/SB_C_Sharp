fb01c90f88b41d02c11d921c8da3cb6eb693b28d
Alexander <49525149+AlexKeyMAD@users.noreply.github.com>
2023-07-17 23:44:17 +0600

    Task 3.4
    finish task

---
+++ Module_03/Task 4. The smallest element in the sequence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4.The_smallest_element_in_the_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину последовательности:");
+            int size = int.Parse(Console.ReadLine());
+
+            int min = int.MaxValue;
+
+            string seq = string.Empty;
+
+            for (int i = 1; i <= size; i++)
+            {
+                Console.WriteLine("Введите число:");
+                var str = Console.ReadLine();
+                
+                seq += str;
+                if (i != size) seq += ", ";
+
+                int num = int.Parse(str);
+
+                min = num < min ? num : min;
+            }
+
+            Console.WriteLine($"Минимальное число последовательности \"{seq}\" - {min}");
+            Console.ReadKey();
+        }
+    }
+}
