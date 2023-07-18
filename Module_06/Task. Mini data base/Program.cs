1a068f851f58958c140c8370cd8abf3a69e6f1e0
Alexander <49525149+AlexKeyMAD@users.noreply.github.com>
2023-07-19 01:04:57 +0600

    Task
    Start task

---
+++ Module_06/Task. Mini data base/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Mini_data_base
+{
+    internal class Program
+    {
+        static string Path = "Data.txt";
+
+        static void ShowData()
+        {
+            Console.Clear();
+
+            if (File.Exists(Path))
+            {
+                string[] list = File.ReadAllLines(Path);
+
+                if (list.Count<string>() == 0) Console.WriteLine("База данных пуста!!!");
+                else
+                {
+                    for (int i = 0; i < list.Length; ++i)
+                    {
+                        Console.WriteLine(list[i]);
+                    }
+                }
+            }
+            else Console.WriteLine("База данных отсутствует!!!");
+
+            Console.ReadKey();
+            
+        }
+
+        static void InsertData()
+        {
+            Console.Clear();
+
+            string[] list = new string[0];
+
+            if (File.Exists(Path))
+            {
+                list = File.ReadAllLines(Path);               
+            }
+            else
+            {
+                File.Create(Path);
+            }
+
+            string[] line = new string[7];
+
+            Console.WriteLine("Введите данные:");
+            Console.Write("Ф.И.О.: ");
+            line[2] = Console.ReadLine();
+            Console.Write("Возраст: ");
+            line[3] = Console.ReadLine();
+            Console.Write("Рост: ");
+            line[4] = Console.ReadLine();
+            Console.Write("Дата рождения: ");
+            line[5] = Console.ReadLine();
+            Console.Write("Место рождения: ");
+            line[6] = Console.ReadLine();
+
+            //ID
+            line[0] = (list.Length + 1).ToString();
+            //Date create
+            line[1] = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
+
+            string str = string.Join("#",line);
+
+            File.AppendAllText(Path,str);
+         
+        }
+
+        static void Main(string[] args)
+        {
+            string str = string.Empty;
+
+            while (str != "0")
+            {
+                Console.Clear();
+                //Меню
+                Console.WriteLine("1 - вывести данные на экран");
+                Console.WriteLine("2 - заполнить данные и добавить новую запись в базу данных");
+                Console.WriteLine("0 - выход");
+                Console.WriteLine("Выберите действие:");
+
+                str = Console.ReadLine();
+
+                switch (str)
+                {
+                    case "1":
+                        ShowData();
+                        break;
+                    case "2":
+                        InsertData();
+                        break;
+                    default:
+                        break;
+                }
+                    
+            }
+        }
+    }
+}
