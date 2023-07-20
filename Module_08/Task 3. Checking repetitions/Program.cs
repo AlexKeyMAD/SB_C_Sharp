acf6fa609416c345659ac7ec6b940181665d2ea2
Alexandr Gussakov <49525149+AlexKeyMAD@users.noreply.github.com>
2023-07-20 16:28:24 +0600

    Task 8.3
    Finished task

---
+++ Module_08/Task 3. Checking repetitions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3.Checking_repetitions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var set = new HashSet<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Введите число:");
+
+                var str = Console.ReadLine();
+
+                if (str == string.Empty) break;
+
+                int num = int.Parse(str);
+
+                if (set.Add(num)) Console.WriteLine("Число добавлено в список");
+                else Console.WriteLine("Число уже присутствует в списке");
+            }
+
+            Console.WriteLine("Список:");
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
