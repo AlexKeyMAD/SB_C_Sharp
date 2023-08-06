014cb51f907915da0451ab60c29cf62cd27033c0
Alexandr Gussakov <it_np@mail.ru>
2023-08-06 19:02:26 +0600

    Task 11
    update

---
+++ Module_11/DataBase/DataBase.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DataBase
+{
+    internal class DataBase
+    {
+        private List<Client> Data = new List<Client>();
+        private List<Consultant> Users = new List<Consultant>();
+        public ref List<Client> data { get { return ref Data; } }
+
+        private string PathData = "database.json";
+        private string PathUsers = "users.json";
+
+        public void Initialosation()
+        {
+            if (File.Exists(PathData))
+            {
+                var json = File.ReadAllText(PathData);
+
+                var _Data = JsonConvert.DeserializeObject<List<Client>>(json);
+
+                if (_Data != null) Data = _Data;
+            }
+
+            if (File.Exists(PathUsers))
+            {
+                var json = File.ReadAllText(PathUsers);
+
+                var _Users = JsonConvert.DeserializeObject<List<Consultant>>(json,new UserConverter());
+
+                if (_Users != null) Users = _Users;
+
+            }
+        }
+
+        ~DataBase()
+        {
+            SaveDataBase();
+        }
+
+        public void SaveDataBase()
+        {
+            var json = JsonConvert.SerializeObject(Data);
+
+            File.WriteAllText(PathData, json);
+        }
+
+        #region Users
+        /// <summary>
+        /// Данный класс создан для правильной десериализации JSON, программа должна понимать какой класс в пользователе использовать
+        /// Этот класс включен в Json.NET с версии 7.0, но у меня версия ниже, поэтому класс здесь
+        /// взято от сюда: https://attach2process.wordpress.com/2017/02/19/deserializing-a-json-to-a-list-of-abstract-types-or-interfaces-with-newtonsoft-json-net-converter/
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public abstract class JsonCreationConverter<T> : JsonConverter
+        {
+            protected abstract T Create(Type objectType, JObject jObject);
+
+            public override bool CanConvert(Type objectType)
+            {
+                return typeof(T) == objectType;
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType,
+                object existingValue, JsonSerializer serializer)
+            {
+                try
+                {
+                    var jObject = JObject.Load(reader);
+                    var target = Create(objectType, jObject);
+                    serializer.Populate(jObject.CreateReader(), target);
+                    return target;
+                }
+                catch (JsonReaderException)
+                {
+                    return null;
+                }
+            }
+
+            public override void WriteJson(JsonWriter writer, object value,
+                JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class UserConverter : JsonCreationConverter<Consultant>
+        {
+            protected override Consultant Create(Type objectType, JObject jObject)
+            {
+                switch ((Roles)jObject["role"].Value<int>())
+                {
+                    case Roles.CONSULTANT:
+                        return new Consultant(jObject["name"].Value<string>());
+                    case Roles.MANAGER:
+                        return new Manager(jObject["name"].Value<string>());
+                }
+                return null;
+            }
+        }
+
+        private void SaveUsers()
+        {
+            var json = JsonConvert.SerializeObject(Users, Formatting.Indented);
+
+            File.WriteAllText(PathUsers, json);
+        }
+
+        private int FindUser(string Name)
+        {
+            for (int i = 0; i < Users.Count; i++)
+            {
+                if (Users[i].GetName() == Name)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public Consultant Identification()
+        {
+            Console.WriteLine("Введите имя пользователя:");
+            var name = Console.ReadLine();
+
+            var fnd = FindUser(name);
+
+            if (fnd == -1)
+            {
+                Console.WriteLine("Выберите роль пользователя:");
+
+                int n = 0;
+
+                foreach (var role in Enum.GetValues(typeof(Roles)))
+                {
+                    n++;
+                    Console.WriteLine($"{n}. {role.ToString()}");
+                }
+
+                int r = int.Parse(Console.ReadLine());
+
+                r = r > n ? 1 : r;
+
+                Consultant usr = null;
+
+                if ((Roles)r == Roles.CONSULTANT) usr = new Consultant(name);
+                else usr = new Manager(name);
+
+                Users.Add(usr);
+
+                SaveUsers();
+
+                return usr;
+            }
+            else
+            {
+                return Users[fnd];
+            }
+        }
+        #endregion
+    }
+}
---
+++ Module_11/DataBase/Log.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    class UnixTimeToDatetimeConverter : DateTimeConverterBase
+    {
+        private static readonly DateTime _epoch =
+            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+
+        public override void WriteJson(JsonWriter writer, object value,
+            JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType,
+            object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+            {
+                return null;
+            }
+
+            return _epoch.AddSeconds(Convert.ToDouble(reader.Value)).ToLocalTime();
+        }
+    }
+
+    [JsonObject(MemberSerialization.Fields)]
+    internal class Log
+    {       
+        [JsonConverter(typeof(UnixTimeToDatetimeConverter))]
+        private DateTime Date { get; set; }
+        private string DataType { get; set; }
+        private string Type { get; set; }
+        private Consultant User { get; set; }
+
+        public Log(string dataType, string type, Consultant user)
+        {
+            this.Date = DateTime.Now;
+            this.DataType = dataType;
+            this.Type = type;
+            this.User = user;
+        }
+
+        public override string ToString()
+        {            
+            return ($"{Date.ToString()}/{DataType}/{Type}/{(User==null?"":User.ToString())}");
+        }
+    }
+}
